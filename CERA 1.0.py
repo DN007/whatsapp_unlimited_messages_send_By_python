ca1fd3ba304cc97014a011956d646bfb2810b851
Dileep Nagendra <gdn.dileep@gmail.com>
2017-08-19 23:18:15 +0530

    Update CERA 1.0.py

--- CERA 1.0.py
+++ CERA 1.0.py
@@ -1,6 +1,6 @@
 import pyautogui as p
 
-x,y=p.locateCenterOnScreen('C:\\Users\\PM\\Desktop\\2.PNG')
+x,y=p.locateCenterOnScreen('C:\\Users\\PM\\Desktop\\yourpng.PNG')
 p.click(x,y)
 
 p.typewrite('BOT: Hii Harikaa !!!!!')
@@ -9,13 +9,7 @@ p.press('enter')
 
 count = 0
 while count < 100:
-    p.typewrite('good night harika')
-    p.press('enter')
-    p.typewrite('sweet dreams')
+    p.typewrite('HAI DUDE! ')
     p.press('enter')
     print(count)
     count += 1
-p.typewrite('Bye harika !!!!!')
-p.press('enter')
-p.typewrite('BOT:I AM CERA 1.0 CREATED BY DILEEP :) ')
-p.press('enter')
